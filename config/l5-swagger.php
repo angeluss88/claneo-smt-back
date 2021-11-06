0ff0d501061ca45a80eba21c4123d27a26942f8b
mrudchenko <mrudchenko@brightgrove.com>
2021-11-07 01:42:41 +0200

    make swagger route configurable

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                 */
-                'api' => 'api/documentation',
+                'api' => env('L5_SWAGGER_ROUTE'),
             ],
             'paths' => [
                 /*
