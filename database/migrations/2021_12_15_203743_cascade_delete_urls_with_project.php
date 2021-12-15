167f3050235eeb12817689771716cd931b46674d
mrudchenko <mrudchenko@brightgrove.com>
2021-12-15 22:49:53 +0200

    add cascade delete urls with projects

---
+++ database/migrations/2021_12_15_203743_cascade_delete_urls_with_project.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CascadeDeleteUrlsWithProject extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('urls', function (Blueprint $table) {
+            $table->bigInteger('project_id', false, true)->change();
+        });
+        Schema::table('urls', function (Blueprint $table) {
+            $table->foreign('project_id')
+                ->references('id')->on('projects')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('urls', function (Blueprint $table) {
+            $table->dropForeign(['urls_project_id_foreign']);
+        });
+    }
+}
